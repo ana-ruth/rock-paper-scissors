647014ada51957b9457b84eb8c0b609c168f4126
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-06-20 16:50:43 -0700

    Step 5: Write the logic to play a single round

--- script.js
+++ script.js
@@ -16,3 +16,60 @@ function getHumanChoice()
 
 let humanScore = 0;
 let computerScore= 0;
+
+function playRound(humanChoice, computerChoice)
+{
+    let human = humanChoice.toLowerCase();
+    let message = " ";
+    
+    if(human == computerChoice) 
+    {
+        message = "It's a tie!"
+        return message;
+    }
+
+    if(human == "rock")
+    {
+        if(computerChoice == "paper") 
+        {
+            message = "You lose! Paper beats Rock.";
+            computerScore +=1;            
+        }
+        if(computerChoice == "scissors")
+        {
+            message = "You win! Rock beats Scissors.";
+            humanScore +=1; 
+        }
+
+    }
+
+    if(human == "paper")
+    {
+        if(computerChoice == "rock") 
+        {
+            message = "You win! Paper beats Rock.";
+            humanScore +=1;            
+        }
+        if(computerChoice == "scissors")
+        {
+            message = "You lose! Scissors beat Paper.";
+            computerChoice +=1; 
+        }
+    }
+
+    if(human == "scissors")
+    {
+        if(computerChoice == "rock") 
+        {
+            message = "You lose! Rock beats scissors.";
+            computerScore +=1;            
+        }
+        if(computerChoice == "paper")
+        {
+            message = "You win! Scissors beat Paper.";
+            humanChoice +=1; 
+        }
+    }
+
+    return message;
+}
